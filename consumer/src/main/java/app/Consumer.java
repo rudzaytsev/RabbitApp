f5962eb2184c22827547cf510674a884081d4e10
rudzaytsev <rudzaytsev@gmail.com>
2017-05-21 15:23:55 +0300

    Initial commit. Added modules for producer and consumer.

---
+++ consumer/src/main/java/app/Consumer.java
@@ -0,0 +1,11 @@
+package app;
+
+/**
+ * Created by rudolph on 21.05.17.
+ */
+public class Consumer {
+
+  public static void main(String[] args) {
+    System.out.println("Hello consumer!");
+  }
+}
---
+++ producer/src/main/java/app/Producer.java
@@ -0,0 +1,10 @@
+package app;
+
+/**
+ * Created by rudolph on 21.05.17.
+ */
+public class Producer {
+  public static void main(String[] args) {
+    System.out.println("Hello Producer!");
+  }
+}
