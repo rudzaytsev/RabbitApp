2c73e926bd6c7f38a6797d4abfc6f475bec33f83
rudzaytsev <rudzaytsev@gmail.com>
2017-05-22 15:58:47 +0300

    Implemented simple producer-consumer example using RabbitMQ messages broker.

--- consumer/src/main/java/app/Consumer.java
+++ consumer/src/main/java/app/Consumer.java
@@ -1,11 +1,36 @@
 package app;
 
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 /**
  * Created by rudolph on 21.05.17.
  */
 public class Consumer {
 
-  public static void main(String[] args) {
+  private final static String QUEUE_NAME = "hello";
+
+  public static void main(String[] args) throws IOException, TimeoutException {
     System.out.println("Hello consumer!");
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+    DefaultConsumer consumer = new DefaultConsumer(channel) {
+      @Override
+      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+        throws IOException {
+        String message = new String(body, "UTF-8");
+        System.out.println(" [x] Received '" + message + "'");
+      }
+    };
+    channel.basicConsume(QUEUE_NAME, true, consumer);
+
   }
 }
--- producer/src/main/java/app/Producer.java
+++ producer/src/main/java/app/Producer.java
@@ -1,10 +1,32 @@
 package app;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 /**
  * Created by rudolph on 21.05.17.
  */
 public class Producer {
-  public static void main(String[] args) {
+
+  private final static String QUEUE_NAME = "hello";
+
+  public static void main(String[] args) throws IOException, TimeoutException {
     System.out.println("Hello Producer!");
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    String message = "Hello World!";
+    channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+    System.out.println(" [x] Sent '" + message + "'");
+
+    channel.close();
+    connection.close();
   }
 }
